699a2d12311627a6e5809b3ffb9c9fd53b3e29ea
00jup <jupark0369@gmail.com>
2024-06-12 16:23:43 +0900

    Line2D class made

--- Vector2D.py
+++ Vector2D.py
@@ -40,17 +40,35 @@ class Vector2D:
             return repr(e)
 
 
+class Line2D:
+    def __init__(self, v1, v2):
+        self.__v1 = v1
+        self.__v2 = v2
+
+    def __len__():
+        pass
+
+    def __eq__(self, other):
+        pass
+
+    def __str__(self):
+        return f"({v1.x}, {v1.y}) - ({v2.x}, {v2.y})"
+
+
 v1 = Vector2D(10, 20)
 v2 = Vector2D(10, 10)
-print(f"{v1} + {v2} = {v1+v2}")
-print(f"{v1} - {v2} = {v1-v2}")
-print(f"{v1} * {v2} = {v1*v2}")
-print(f"{v1} / {v2} = {v1/v2}")
-print(f"-{v1} = {-v1}")
-print(f"{v1} == {v2} = {v1 == v2}")
-print(f"{v1} == {v1} = {v1 == v1}")
-print(f"{v1} == {10} = {v1 == 10}")
+# print(f"{v1} + {v2} = {v1+v2}")
+# print(f"{v1} - {v2} = {v1-v2}")
+# print(f"{v1} * {v2} = {v1*v2}")
+# print(f"{v1} / {v2} = {v1/v2}")
+# print(f"-{v1} = {-v1}")
+# print(f"{v1} == {v2} = {v1 == v2}")
+# print(f"{v1} == {v1} = {v1 == v1}")
+# print(f"{v1} == {10} = {v1 == 10}")
 
 print(f"{v1}와 {v2}사이의 거리 : {Vector2D.distance(v1, v2)}")
+
+line1 = Line2D(v1, v2)
+print(line1)
 
 print("20192294 박정욱")
