bc2b85f4197d7183b38cd4a0ad80714e59a71fc1
00jup <jupark0369@gmail.com>
2024-06-12 16:33:45 +0900

    equal in Line2D

--- Vector2D.py
+++ Vector2D.py
@@ -49,10 +49,16 @@ class Line2D:
         return int(math.sqrt(math.pow(self.__v1.x - self.__v2.x, 2) + math.pow(self.__v1.y - self.__v2.y, 2)))
 
     def __eq__(self, other):
-        pass
+        if isinstance(other, Line2D):
+            if self.__v1 == other.__v1 and self.__v2 == other.__v2:
+                return True
+            else:
+                return False
+        else:
+            return False
 
     def __str__(self):
-        return f"({v1.x}, {v1.y}) - ({v2.x}, {v2.y})"
+        return f"({self.__v1.x}, {self.__v1.y}) - ({self.__v2.x}, {self.__v2.y})"
 
 
 v1 = Vector2D(10, 20)
@@ -69,7 +75,11 @@ v2 = Vector2D(10, 10)
 print(f"{v1}와 {v2}사이의 거리 : {Vector2D.distance(v1, v2)}")
 
 line1 = Line2D(v1, v2)
+line2 = Line2D(v2, v1)
 print(line1)
 print(len(line1))
+
+print(line1 == line1)
+print(line1 == line2)
 
 print("20192294 박정욱")
